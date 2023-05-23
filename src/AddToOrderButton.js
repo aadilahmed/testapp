fbc0120c8ac67fde5507b66dcbc693c974a069ad
Aadil Ahmed <aadilahmed0@gmail.com>
2023-05-23 03:25:52 -0700

    move addtoorder and nav

---
+++ src/AddToOrderButton.js
@@ -0,0 +1,11 @@
+const AddToOrderButton = () => {
+    const buttonText = 'Add To Order';
+    return (
+      <>
+        <button>{buttonText}</button>
+        <br /><br />
+      </>
+    )
+  }
+
+  export default AddToOrderButton
